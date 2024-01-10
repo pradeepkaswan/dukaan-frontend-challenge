76c435fb9a4641a0e0c2e5dde273aebaf8a20158
Pradeep Kaswan <pradeepkaswan7@gmail.com>
2024-01-10 12:37:53 +0530

    feat: implement header

--- src/components/layouts/root-layout.tsx
+++ src/components/layouts/root-layout.tsx
@@ -1,16 +1,14 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Nav from '../sections/nav'
 import Header from '../sections/header'
 
 export default function RootLayout() {
   return (
     <div className="bg-background flex relative">
-      <div id="sidebar">
-        <Nav />
-        <div className="w-full">
-          <Header />
-          <Outlet />
-        </div>
+      <Nav />
+      <div className="w-full">
+        <Header />
+        <Outlet />
       </div>
     </div>
   )
--- src/components/sections/nav.tsx
+++ src/components/sections/nav.tsx
@@ -1,21 +1,65 @@
 import { navLinks } from '@/assets/data'
 import { Link } from 'react-router-dom'
+import { ChevronDown } from 'lucide-react'
+import UserImage from '@/assets/images/user.png'
 
 const Nav = () => {
   return (
-    <aside className="fixed left-0 top-0 ">
+    <aside className="fixed left-0 top-0 z-10 h-screen w-full overflow-hidden transition-all duration-300 pointer-events-all opacity-100">
       <div className="relative mr-auto transition-transform duration-300 ease-in-out flex flex-col max-w-[224px] px-2 py-4 h-[100dvh] gap-4 bg-[#1E2640] justify-between translate-x-0">
-        <div className="w-full flex-col gap-4">
-          {navLinks.map((link) => (
-            <Link
-              key={link.label}
-              to="/home"
-              className="bg-[#FFFFFF]/10 px-4 py-2 flex gap-3 text-[#D2D4D9] items-center rounded-[4px] hover:bg-[#FFFFFF]/10"
-            >
-              <div className="object-contain object-center">{link.icon}</div>
-              <p className="text-[14px] font-medium">{link.label}</p>
-            </Link>
-          ))}
+        <div className="w-full flex flex-col gap-6 items-center">
+          <div className="flex gap-3 text-[#FFFFFF] items-center w-fit mx-auto">
+            <div className=" bg-white w-[39px] h-[39px] m-[0.5px] rounded-[4px] relative">
+              <img src={UserImage} alt="user" />
+            </div>
+            <div className="flex flex-col gap-1 w-[108px]">
+              <h3 className="font-medium text-[15px]">Nishyan</h3>
+              <Link
+                to={'#'}
+                className="underline underline-offset-2 text-[13px] font-light"
+              >
+                Visit Store
+              </Link>
+            </div>
+            <ChevronDown />
+          </div>
+          <div className="w-full flex-col gap-4">
+            {navLinks.map((link) => (
+              <Link
+                key={link.label}
+                to={link.to}
+                className="px-4 py-2 flex gap-3 text-[#D2D4D9] items-center rounded-[4px] hover:bg-[#FFFFFF]/10"
+              >
+                <div className="object-contain object-center">{link.icon}</div>
+                <p className="text-[14px] font-medium">{link.label}</p>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="py-[6px] px-3 bg-[#353C53] mx-2 rounded-[4px]">
+          <div className="flex gap-3 items-center">
+            <div className="w-[36px] h-[36px] bg-[#FFFFFF]/10 rounded-[4px] p-[6px]">
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M3.0002 1.79727C2.42343 1.79727 1.8002 2.35527 1.8002 3.24727V16.7473C1.8002 17.6393 2.42343 18.1973 3.0002 18.1973H21.0002C21.577 18.1973 22.2002 17.6393 22.2002 16.7473V13.8473H17.0003C14.874 13.8473 13.1503 12.1236 13.1503 9.99727C13.1503 7.87097 14.874 6.14727 17.0003 6.14727H22.2002V3.24727C22.2002 2.35527 21.577 1.79727 21.0002 1.79727H3.0002ZM23.8002 6.14727V3.24727C23.8002 1.65398 22.6326 0.197266 21.0002 0.197266H3.0002C1.36782 0.197266 0.200195 1.65398 0.200195 3.24727V16.7473C0.200195 18.3405 1.36782 19.7973 3.0002 19.7973H21.0002C22.6326 19.7973 23.8002 18.3405 23.8002 16.7473V13.8473H23.8503V6.14727H23.8002ZM16.0002 9.94727C16.0002 9.47783 16.3808 9.09727 16.8502 9.09727H18.1502C18.6197 9.09727 19.0002 9.47783 19.0002 9.94727C19.0002 10.4167 18.6197 10.7973 18.1502 10.7973H16.8502C16.3808 10.7973 16.0002 10.4167 16.0002 9.94727ZM14.8503 9.99727C14.8503 8.80986 15.8129 7.84727 17.0003 7.84727H22.1503V12.1473H17.0003C15.8129 12.1473 14.8503 11.1847 14.8503 9.99727Z"
+                  fill="white"
+                />
+              </svg>
+            </div>
+
+            <div className="flex flex-col text-white gap-[2px]">
+              <h2 className="text-[13px]">Available credits</h2>
+              <h6 className="text-base font-normal">222.10</h6>
+            </div>
+          </div>
         </div>
       </div>
     </aside>
